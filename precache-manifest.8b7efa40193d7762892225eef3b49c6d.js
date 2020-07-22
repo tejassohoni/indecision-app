f1b5f246b7194be2a36d65ff63606b2931a3e1e7
tejas1597 <tjs1597@gmail.com>
2020-07-22 18:50:46 +0530

    Updates

---
+++ precache-manifest.8b7efa40193d7762892225eef3b49c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a3a054fb938ac5729d01b158fee330",
+    "url": "/indecision-app/index.html"
+  },
+  {
+    "revision": "67cdf12da0138ccd6b99",
+    "url": "/indecision-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a3f086a8f03b0798ae4a",
+    "url": "/indecision-app/static/css/main.174ea75c.chunk.css"
+  },
+  {
+    "revision": "67cdf12da0138ccd6b99",
+    "url": "/indecision-app/static/js/2.69a524ba.chunk.js"
+  },
+  {
+    "revision": "ed901023237d66f16963aae8c7889084",
+    "url": "/indecision-app/static/js/2.69a524ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f086a8f03b0798ae4a",
+    "url": "/indecision-app/static/js/main.d635dc3f.chunk.js"
+  },
+  {
+    "revision": "38af5a2ddbbfc61265ee",
+    "url": "/indecision-app/static/js/runtime-main.849a4ea2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46f8323f296d414a2b33c592a8f85a3c.js"
+  "/indecision-app/precache-manifest.8b7efa40193d7762892225eef3b49c6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/indecision-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
