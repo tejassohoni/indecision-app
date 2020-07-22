49517d66581ea7760cc5898a0e57c5b00ce6182c
tejas1597 <tjs1597@gmail.com>
2020-07-22 18:50:07 +0530

    Updates

---
+++ precache-manifest.46f8323f296d414a2b33c592a8f85a3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a57ec721d5493e003db6ab5b125d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67cdf12da0138ccd6b99",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6ca1159fbf6bf61e676b",
+    "url": "/static/css/main.174ea75c.chunk.css"
+  },
+  {
+    "revision": "67cdf12da0138ccd6b99",
+    "url": "/static/js/2.69a524ba.chunk.js"
+  },
+  {
+    "revision": "ed901023237d66f16963aae8c7889084",
+    "url": "/static/js/2.69a524ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca1159fbf6bf61e676b",
+    "url": "/static/js/main.12049b43.chunk.js"
+  },
+  {
+    "revision": "6a968a62a5ec5f2f4499",
+    "url": "/static/js/runtime-main.1a4c9a59.js"
+  }
+]);
